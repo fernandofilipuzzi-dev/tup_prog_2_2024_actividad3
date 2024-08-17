151c9ed11320a6917fd750e0d63deb52428a648a
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-08-17 13:51:49 -0300

    iniciando el repo copiando la activad 2 y renomanbrandola a activiad3

---
+++ Actividad3/Actividad/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Persona
+    {
+        public int DNI { get; private set; }
+        public string Nombre { get; private set; }
+
+        public Persona(int dni, string nombre)
+        {
+            this.DNI = dni;
+            this.Nombre = nombre;
+        }
+
+        public string VerDatos()
+        {
+            return $"{this.Nombre}(DNI:{this.DNI})";
+        }
+    }
+}
---
+++ Actividad3/Actividad/Models/Policia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Policia
+    {
+        public int NumeroPlaca { get; private set; }
+        public int DNI { get; private set; }
+        public string Nombre { get; private set; }
+        
+        public Policia(int dni, string nombre, int numeroPlaca)
+        {
+            this.DNI = dni;
+            this.Nombre = nombre;
+            this.NumeroPlaca = numeroPlaca;
+        }
+
+        public string VerDatos() 
+        {
+            return $"{this.Nombre}(DNI:{this.DNI} - placa:{NumeroPlaca})";
+        }
+    }
+}
